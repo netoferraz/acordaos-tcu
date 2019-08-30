ceb436822f6142d91fb9c035cbbc50eb6e465a10
josefn <jose.ferraz@presidencia.gov.br>
2019-08-30 14:55:46 -0300

    aprimora parser de log

--- scripts/parse_log.py
+++ scripts/parse_log.py
@@ -1,14 +1,15 @@
 #escrever um arquivo com as urn que geraram erro no crawler
 import re
 pattern = r'\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\sERROR\s\[scrapy.core.scraper\]\sSpider error processing\s<GET https?:\/\/.*[\r\n]*'
-
-logfilename = 'log_apiacordao_14.txt'
+find_urn = 'urn:lex:br:.+'
+logfilename = 'log_apiacordao_15.txt'
 with open(f"./logs/{logfilename}", 'r', encoding='utf8') as f:
-    with open("./logs/urns_to_delete_2.log", "w", encoding='utf8') as _:
+    with open("./logs/urns_to_delete_3.log", "w", encoding='utf8') as _:
         d = f.readlines()
         for line in d:
             l = line.replace("\n", "")
             find_error = re.search(pattern, l)
             if find_error:
-                error_urn = find_error.group(0).split("/")[4].split(">")[0]
+                split_str = find_error.group(0).split("/")
+                error_urn = [text for text in split_str if re.search(find_urn, text)][0][:-1]
                 _.write(f"{error_urn}\n")
